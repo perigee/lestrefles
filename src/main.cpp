556d7b1c5cce4776a2b5108f8836c6e4900ed8d4
perigee <junhubbs@gmail.com>
2012-11-01 19:50:42 +0000

    add src directory

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+//#include <boost/thread/thread.hpp>
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <thread>
+//#include <boost/move/move.hpp>
+ 
+void hello(int i)
+{
+  std::cout <<
+    "Hello world, I'm a thread! "
+	    << i 
+  << std::endl;
+}
+ 
+int main(int argc, char* argv[])
+{
+  
+  std::vector<std::thread> workers;
+  
+  for (int i=0; i<8; ++i){
+    auto th = std::thread(&hello, i);
+    workers.push_back(std::move(th));
+
+  }
+
+  std::cout << "Hi from main\n" ;
+
+  std::for_each(workers.begin(),workers.end(), [](std::thread & th){th.join();});
+
+  return 0;
+}
