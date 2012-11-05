4c9e2a2f1a0183f48def0d521709a2a15247d578
perigee <junhubbs@gmail.com>
2012-11-05 20:39:11 +0000

    add self file

--- src/R/nvidia.r
+++ src/R/nvidia.r
@@ -17,14 +17,14 @@ dbDisconnect(con)
 for(i in 1:length(tickers[,1])) {
   ticker=tickers[i,1]
   #Get date
-  beginDate <- '2000-01-01'
+  beginDate <- '1990-01-01'
   today <- Sys.Date()
   format(today, format="%Y-%m-%d")
 
 
   
-#data <- getSymbols(ticker, from=beginDate, to=today, auto.assign=FALSE)
-#data <- adjustOHLC(data, use.Adjusted=TRUE)
+data <- getSymbols(ticker, from=beginDate, to=today, auto.assign=FALSE)
+data <- adjustOHLC(data, use.Adjusted=TRUE)
 #filename <- c("cache/",tickers[1,1],".rdata")
 #save(data,file=filename)
 chartSeries(data,name=ticker, subset='last 13 months', TA="addMACD();addBBands()")
